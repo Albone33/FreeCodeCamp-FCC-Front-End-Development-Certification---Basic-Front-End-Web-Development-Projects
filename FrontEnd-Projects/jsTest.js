fc15e0d6898fdc7e3e78a30f4421171031652e86
Wasiu Adisa <aowasiu@gmail.com>
2017-07-16 02:59:39 +0100

    Add files via upload

---
+++ FrontEnd-Projects/jsTest.js
@@ -0,0 +1,11 @@
+
+
+				var oldArray = [1,2,3,4,5];
+			
+				var arrayPlusThree = oldArray.map(
+					function (val) {
+						return val + 3;
+					}
+				);
+				
+				conlose.log(arrayPlusThree);
